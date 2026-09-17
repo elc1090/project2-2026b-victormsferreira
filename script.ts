c9df3de2f98580b85782ad4e687977176ce29c22
victormsferreira <115111648+victormsferreira@users.noreply.github.com>
2026-09-16 21:24:02 -0300

    Update script.ts

--- script.ts
+++ script.ts
@@ -320,9 +320,9 @@ class Entity {
       true , true , false, true , true , true , true , true , false, false, false, false, false, false, false, false,
       true , true , true , true , true , true , true , true , false, false, false, false, false, false, true , true ,
       true , true , true , true , false, false, true , true , true , true , false, true , true , true , true , true ,
-      true , true , true , true , false, false, false, false, false, false, false, true , true , false, true , true ,
+      true , true , true , true , true , true , true , false, false, false, false, true , true , false, true , true ,
       false, false, false, false, false, false, false, false, false, false, false, true , true , true , true , true ,
-      false, false, false, false, true , true , true , true , true , true , false, true , true , true , true , true ,
+      false, false, false, false, true , true , true , true , false, false, false, true , true , true , true , true ,
       false, false, false, false, false, false, false, false, false, false, false, true , true , true , true , true ,
       true , true , false, true , true , false, true , true , false, true , true , true , true , true , true , true ,
     ]
