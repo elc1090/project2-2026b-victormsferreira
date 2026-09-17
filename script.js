162c5905ff8ce1a538d6a1351ccd0b5c4c3fcfb2
victormsferreira <115111648+victormsferreira@users.noreply.github.com>
2026-09-16 21:24:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -316,9 +316,9 @@ class Entity {
             true, true, false, true, true, true, true, true, false, false, false, false, false, false, false, false,
             true, true, true, true, true, true, true, true, false, false, false, false, false, false, true, true,
             true, true, true, true, false, false, true, true, true, true, false, true, true, true, true, true,
-            true, true, true, true, false, false, false, false, false, false, false, true, true, false, true, true,
+            true, true, true, true, true, true, true, false, false, false, false, true, true, false, true, true,
             false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, true,
-            false, false, false, false, true, true, true, true, true, true, false, true, true, true, true, true,
+            false, false, false, false, true, true, true, true, false, false, false, true, true, true, true, true,
             false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, true,
             true, true, false, true, true, false, true, true, false, true, true, true, true, true, true, true,
         ];
